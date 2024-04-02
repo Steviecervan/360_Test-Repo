e4475991eff224a29da778877de3864594ed90d9
steviecervantes <stevie.cervan06@gmail.com>
2024-04-02 14:37:13 -0700

    Stevie's Second Commit

--- src/helloworld/test.java
+++ src/helloworld/test.java
@@ -1,7 +1,26 @@
 package helloworld;
 
-public class test {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class test extends Application{
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		launch(args);
+	}
+	
+	public void start(Stage stage) {		
+		stage.setTitle("In Class GitHub Activity");
+		BorderPane root = new BorderPane();
+		Button nameButton = new Button("Stevie's second commit");
+		
+		root.setCenter(nameButton);	
+		//root.getChildren().add(nameButton);
+		stage.setScene(new Scene(root, 650, 400));
+		stage.show();
 	}
 }
